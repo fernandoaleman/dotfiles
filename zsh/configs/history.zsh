8a61e08d79b9aa891cc268a6812ecd5403933d59
Fernando Alemán <fernandoaleman@mac.com>
2024-09-15 18:23:12 -0500

    Update zsh configs

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -2,5 +2,13 @@ setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zhistory
 HISTSIZE=4096
 SAVEHIST=4096
+setopt share_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_verify
+
+# completion using arrow keys (based on history)
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
 
 export ERL_AFLAGS="-kernel shell_history enabled"
