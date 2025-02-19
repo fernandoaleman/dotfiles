ed3c674dd60041c23c45ba4c5d89339e38a8eca3
Fernando Alemán <fernandoaleman@mac.com>
2025-02-19 08:39:06 -0600

    Fix lazyvim for older version of ruby < 3

--- zsh/configs/asdf.zsh
+++ zsh/configs/asdf.zsh
@@ -7,22 +7,33 @@ export OPENSSL_CFLAGS="-Wno-error=implicit-function-declaration"
 export CFLAGS=-Wno-error="implicit-function-declaration"
 
 function chpwd {
-
-  # Check if a .ruby-version file exists
   if [[ -f "$PWD/.ruby-version" ]]; then
-    # ruby_version=`cat .ruby-version`
     ruby_version=$(<.ruby-version)
     ruby_version="${ruby_version#*-}"
 
-    if ! asdf list ruby | grep -q $ruby_version; then
+    # Check if the Ruby version is installed with asdf
+    if ! asdf list ruby | grep -q "$ruby_version"; then
       while true; do
-        read REPLY\?"Install ruby $ruby_version? [Yn] "
+        read -p "Install ruby $ruby_version? [Yn] " REPLY
         case $REPLY in
-          [Yy]* ) asdf install ruby $ruby_version; gem update --system; break;;
+          [Yy]* ) asdf install ruby "$ruby_version"; gem update --system; break;;
           [Nn]* ) break;;
           * ) echo "Please answer y or n.";;
         esac
       done
+    fi
+
+    # Extract the major version number
+    ruby_major_version="${ruby_version%%.*}"
+
+    # Fix lazyvim for older versions of Ruby < 3
+    # Check if the ruby version is less than 3 and the symlink doesn't already exist
+    if [[ "$ruby_major_version" -lt 3 && ! -e "$PWD/.lazy.lua" ]]; then
+      # Check if ~/.lazy.lua file or symlink exists
+      if [[ -e "$HOME/.lazy.lua" || -L "$HOME/.lazy.lua" ]]; then
+        ln -sf "$HOME/.lazy.lua" "$PWD/.lazy.lua"
+        echo "Symlink to ~/.lazy.lua created in $PWD"
+      fi
     fi
   fi
 }
